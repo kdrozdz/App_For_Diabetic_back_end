4f715c98fb0b5fb8aae949117dc9b5c05dd7cbc2
KD <kdrozdzdev@gmail.com>
2020-10-20 21:13:13 +0200

    test authenticated

--- Api/tests/test_authenticated.py
+++ Api/tests/test_authenticated.py
@@ -1,17 +1,15 @@
-from django.test import TestCase
-import json
-from rest_framework.exceptions import ErrorDetail
 from accounts.models import Account
-from django.urls import reverse
 from rest_framework.authtoken.models import Token
 from rest_framework.test import APITestCase
 from rest_framework import status
-from Api.tests.data import account_data
 
 
 class TestAuthenticatedViews(APITestCase):
 
-    url_account = '/accounts/'
+    url_accounts = '/accounts/'
+    url_patient = '/patient/'
+    url_doctors = '/doctors/'
+
 
     def setUp(self):
         self.account = Account.objects.create_user(
@@ -29,14 +27,30 @@ class TestAuthenticatedViews(APITestCase):
     def api_authentication(self):
         self.client.credentials(HTTP_AUTHORIZATION="Token " + self.token.key)
 
+
     def test_accounts_authenticated(self):
-        response = self.client.get(self.url_account)
+        response = self.client.get(self.url_accounts)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_accounts_un_authenticated(self):
         self.client.force_authenticate(user=None)
-        response = self.client.get(self.url_account)
+        response = self.client.get(self.url_accounts)
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
+    def test_patient_authenticated(self):
+        response = self.client.get(self.url_accounts)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
+    def test_patient_un_authenticated(self):
+        self.client.force_authenticate(user=None)
+        response = self.client.get(self.url_accounts)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
+    def test_doctors_authenticated(self):
+        response = self.client.get(self.url_accounts)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_doctors_un_authenticated(self):
+        self.client.force_authenticate(user=None)
+        response = self.client.get(self.url_accounts)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
