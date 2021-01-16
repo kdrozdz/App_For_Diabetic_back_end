19490ca05ff44486351d3b31f28b600de2b8ffab
KD <kdrozdzdev@gmail.com>
2021-01-17 00:02:09 +0100

    admin models register

--- Api/admin.py
+++ Api/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
-from .models import Patient, Doctor
+from .models import Patient, Doctor, Food, Advice, Chat, SugarLevel, Account, Cooperate, RejectCooperate
 
 admin.site.register(Patient)
 admin.site.register(Doctor)
+admin.site.register(Food)
+admin.site.register(Advice)
+admin.site.register(Chat)
+admin.site.register(SugarLevel)
+admin.site.register(Account)
+admin.site.register(Cooperate)
+admin.site.register(RejectCooperate)
